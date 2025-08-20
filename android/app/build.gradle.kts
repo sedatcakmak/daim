b22da2e95aa267ef077a630485e60532ec6a21aa
sedattr <xsedattr@gmail.com>
2025-08-20 22:52:43 +0300

    firebase fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.cakmakstudios.daim"
         minSdk = 23
         targetSdk = 36
-        versionCode = 21
+        versionCode = 22
         versionName = "1.0.0"
     }
 
